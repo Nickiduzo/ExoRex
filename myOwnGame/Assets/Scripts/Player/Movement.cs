dfb24d01e5e7c619f3c5f76110618ce564e7b95d
vellansole <79377105+Nickiduzo@users.noreply.github.com>
2023-09-20 00:02:14 +0300

    Nikita - Mine Scene (#18)
    * GitHub | Input
    
    Added new information to .md-files
    
    * GitHub
    
    Added new information into "GitHub"
    
    * Link
    
    * nelink
    
    * Input
    
    * New Files
    
    * Plaggins
    
    Install plaggins for unity2d(Visual Studio Code 2022)
    
    * Refactoring
    
    * Optimization
    
    Refactoring code and deleted many unusless things
    
    * Main Scene
    
    * Instruction
    
    * NewImage
    
    * Animations
    
    * Test | Animation
    
    Added new animation for player
    
    * Animation | MP | ME
    
    New animation for main player and main enemy
    
    * Hierarchy
    
    New hierarchy
    
    * Graphics | Scene
    
    New scene and new graphic
    
    * Mine Scene | Move character
    
    Added new animation and moving to new scene

--- myOwnGame/Assets/Scripts/Player/Movement.cs
+++ myOwnGame/Assets/Scripts/Player/Movement.cs
@@ -5,6 +5,7 @@ public class Movement : MonoBehaviour
     [SerializeField] private float speed = 140f;
     [SerializeField] private Rigidbody2D rb;
 
+    private bool isFacingLeft = false;
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -12,6 +13,23 @@ public class Movement : MonoBehaviour
 
     public void Move(float horizontalDirection)
     {
+        if (horizontalDirection < 0 && !isFacingLeft)
+        {
+            Flip();
+        }
+        // Если персонаж двигается вправо и он ранее был направлен влево, выполнить поворот
+        else if (horizontalDirection > 0 && isFacingLeft)
+        {
+            Flip();
+        }
+
         rb.velocity = new Vector2(horizontalDirection * speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+    private void Flip()
+    {
+        isFacingLeft = !isFacingLeft;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
