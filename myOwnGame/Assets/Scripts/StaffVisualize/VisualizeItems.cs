1cfcbef8ddd9a6800f519142937aa9bbecfd6105
Nickiduzo <79377105+Nickiduzo@users.noreply.github.com>
2024-07-22 23:00:53 +0300

    Clear
    - clear all assets

--- myOwnGame/Assets/Scripts/StaffVisualize/VisualizeItems.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class VisualizeItems : MonoBehaviour
-{
-    [SerializeField]
-    Dictionary<string,bool> mineralAbility = new Dictionary<string,bool>();
-}
